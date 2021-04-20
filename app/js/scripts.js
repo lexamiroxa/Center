063b0e913a863d4c94b80eb2326fd03c559f551c
Денис Егоров <NeutralVVinter@gmail.com>
2021-04-20 14:56:34 +0300

    События при скролле и ресайзе

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,15 +2,30 @@ window.onscroll = function() {ScrollEvent()};
 var HeaderMod = document.querySelector('.Header');
 var MainElem = document.querySelector('main');
 
+window.addEventListener('resize', function() {
+    ScrollEvent();
+});
+
 function ScrollEvent() {
-    if (document.body.scrollTop > 184 || document.documentElement.scrollTop > 184) {
+    if (window.innerWidth > 768 && document.documentElement.scrollTop > 184) {
         HeaderMod.style.position = 'fixed';
         HeaderMod.style.marginTop = '-184px';
-        MainElem.style.paddingTop = '233px'
-    } else {
+        MainElem.style.paddingTop = '233px';
+    } 
+    else if (window.innerWidth > 540 && window.innerWidth <= 768 && document.documentElement.scrollTop > 172) {
+        HeaderMod.style.position = 'fixed';
+        HeaderMod.style.marginTop = '-172px';
+        MainElem.style.paddingTop = '222px';
+    } 
+    else if (window.innerWidth <= 540 && document.documentElement.scrollTop > 74) {
+        HeaderMod.style.position = 'fixed';
+        HeaderMod.style.marginTop = '-74px';
+        MainElem.style.paddingTop = '114px';
+    } 
+    else {
         HeaderMod.style.position = 'static';
         HeaderMod.style.marginTop = '0';
-        MainElem.style.paddingTop = '0'
+        MainElem.style.paddingTop = '0';
     }
 }
 
@@ -21,22 +36,12 @@ document.querySelector('.NavMore').addEventListener('click', () => {
         .forEach(span => span.classList.toggle('NavMoreActive'));
 });
     
-    
-
-
-// document.addEventListener('click', (evt) => {
-//     var HeaderElement = document.querySelector('.Header');
-//     let targetElement = evt.target;
-
-//     do {
-//         if (targetElement == HeaderElement) {
-//             return;
-//         }
-//         targetElement = targetElement.parentNode;
-//     } while (targetElement);
-
-//     document.querySelectorAll('.SecondNavigation')
-//         .forEach(div => div.classList.toggle('SecondNavigationOpen'));
-//     document.querySelectorAll('.NavMoreText')
-//         .forEach(span => span.classList.toggle('NavMoreActive'));
-// });
+document.addEventListener('click', (e) => {
+    var container = document.querySelector('.Header');
+    if (!container.contains(e.target) && document.querySelector('.SecondNavigation').classList.contains('SecondNavigationOpen')) {
+        document.querySelectorAll('.SecondNavigation')
+            .forEach(div => div.classList.toggle('SecondNavigationOpen'));
+        document.querySelectorAll('.NavMoreText')
+            .forEach(span => span.classList.toggle('NavMoreActive'));
+    }
+});
